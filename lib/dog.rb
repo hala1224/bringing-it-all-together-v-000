ce555d013ad68aabf6ac8fdace849ea7068f17b6
Natalie Halawani <hala1224@gmail.com>
2019-02-24 02:17:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,4 +14,15 @@ class Dog
         self.breed
     end
     
+    def self.create_table
+      sql = <<-SQL
+       CREATE TABLE IF NOT EXISTS dogs (
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       breed TEXT
+       )
+       SQL 
+       DB[:conn].execute(sql)
+     end
+    
   end
